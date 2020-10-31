aeabb60ff03d928e3e4166d3c6be81c080baf081
Julia Kernitckaya <kernyusha@yandex.ru>
2020-10-31 06:44:06 +0300

    Init project

---
+++ README.md
@@ -0,0 +1,1 @@
+AQA-Homework-2 [![Build status](https://ci.appveyor.com/api/projects/status/p8fypk8hrp6raal0/branch/master?svg=true)](https://ci.appveyor.com/project/Kernitskaya/aqa-homework-2/branch/master)
---
+++ src/test/java/ru/netology/rest/PostRequestTest.java
@@ -0,0 +1,21 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostRequestTest {
+    @Test
+    void testPostRequest() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some value")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some value"))
+        ;
+    }
+}
